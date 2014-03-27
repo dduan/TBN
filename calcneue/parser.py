4ea0bddfe9ae2f526ad15f7aa53a2e53ff735fe6
hmb42 <hmb42@txstate.edu>
2014-03-27 17:50:19 -0500

    added tests and implemented parser

---
+++ calcneue/parser.py
@@ -0,0 +1,96 @@
+from __future__ import print_function, unicode_literals, division
+
+import ply.yacc as yacc
+try:
+    from calcneue.lexer import CalcNeueLexer
+except:
+        from lexer import CalcNeueLexer
+
+class CalcNeueParser:
+
+    precedence = (
+        ('left', '='),
+        ('left', '+', '-'),
+        ('left', '*', '/', '%'),
+        ('right', '^'),
+        ('right', 'UMINUS'),
+    )
+
+    def __init__(self):
+        self.lexer = CalcNeueLexer()
+        self.tokens = self.lexer.tokens
+        self.parser = yacc.yacc(module=self)
+
+
+    def p_binop_plus(self, p):
+        'expression   : expression "+" expression'
+        p[0] = ('binop_plus', p[1], p[3])
+
+    def p_binop_minus(self, p):
+        'expression   : expression "-" expression'
+        p[0] = ('binop_minus', p[1], p[3])
+
+    def p_binop_multiply(self, p):
+        'expression   : expression "*" expression'
+        p[0] = ('binop_multiply', p[1], p[3])
+
+    def p_binop_divide(self, p):
+        'expression   : expression "/" expression'
+        p[0] = ('binop_divide', p[1], p[3])
+
+    def p_binop_mod(self, p):
+        'expression   : expression "%" expression'
+        p[0] = ('binop_mod', p[1], p[3])
+
+    def p_binop_power(self, p):
+        'expression   : expression "^" expression'
+        p[0] = ('binop_power', p[1], p[3])
+
+    def p_expression_paren(self, p):
+        '''expression   : "(" expression ")"'''
+        #p[0] = ('group_expr', p[2])
+        p[0] = p[2]
+
+    def p_assignment(self, p):
+        "expression :   IDENTIFIER '=' expression"
+        p[0] = ('assignment', p[3], p[1])
+
+    def p_negative_expr(self, p):
+        "expression :   - expression %prec UMINUS"
+        p[0] = ('negative_expr', p[2])
+
+    def p_expression_unit_convert(self, p):
+        '''expression   :   expression IN IDENTIFIER'''
+        p[0] = ('convert_expr', p[1], p[3])
+
+    def p_expression_function(self, p):
+        "expression     :   IDENTIFIER '(' expression ')' "
+        p[0] = ('function_expr', p[3], p[1])
+
+    def p_expression_quantity(self, p):
+        '''expression   :   number IDENTIFIER
+                        |   number'''
+        try:
+            p[0] = ('quantity', p[1], p[2])
+        except IndexError:
+            p[0] = ('quantity', p[1], None)
+
+    def p_number_float(self, p):
+        '''number : FLOAT'''
+        p[0] = ('float_number', p[1])
+
+    def p_number_integer(self, p):
+        '''number : INTEGER'''
+        p[0] = ('integer_number', p[1])
+
+    def p_error(self, p):
+        print("syntax error: {}".format(p))
+
+if __name__ == '__main__':
+    calc = CalcNeueParser()
+    while True:
+        try:
+            s = input('calc > ')
+        except EOFError:
+            break
+        print(calc.parser.parse(s))
---
+++ tests/test_parser.py
@@ -0,0 +1,123 @@
+import pytest
+from calcneue.parser import CalcNeueParser
+from helpers import _i, _f, input_should_match
+
+@pytest.fixture
+def calc(): return CalcNeueParser().parser.parse
+
+def test_number_only_quantity(calc):
+    input_should_match(calc, {
+        '42': _i(42),
+        '0o3': _i(3),
+        '0': _i(0),
+        '3e2': _f(3e2),
+        '0.3': _f(0.3),
+    })
+
+def test_unit_quantity(calc):
+    input_should_match(calc, {
+        '2 inch': _i(2, 'inch'),
+        '3e7 km': _f(3e7, 'km'),
+        '0 kblam': _i(0, 'kblam'),
+    })
+
+def test_function_expr(calc):
+    input_should_match(calc, {
+        'sin(0.4)': ('function_expr', _f(0.4), 'sin'),
+        'blow(0.4)': ('function_expr', _f(0.4), 'blow'),
+        'wat(1+2)': (
+            'function_expr',
+            ('binop_plus', _i(1), _i(2),),
+            'wat'
+        ),
+    })
+
+def test_convert_expr(calc):
+    input_should_match(calc, {
+        '1 in meters': ( 'convert_expr', _i(1), 'meters'),
+        '1 inch in meters':('convert_expr', _i(1, 'inch'), 'meters'),
+        '1 inch + 2 in meters': (
+            'convert_expr',
+            ('binop_plus', _i(1, 'inch'), _i(2)),
+            'meters'
+        ),
+        '1 inch in meters in lightyears': (
+            'convert_expr',
+            ('convert_expr', _i(1, 'inch'), 'meters'),
+            'lightyears'
+        ),
+    })
+
+def test_group_expr(calc):
+    input_should_match(calc, {
+        '1 + ( 2 + 3 )': ('binop_plus', _i(1), ('binop_plus', _i(2), _i(3))),
+        '1 / ( 2 + 3 )': ('binop_divide', _i(1), ('binop_plus', _i(2), _i(3))),
+        '( 2 + 3 ) / 1': ('binop_divide', ('binop_plus', _i(2), _i(3)), _i(1)),
+    })
+
+def test_binop_power(calc):
+    input_should_match(calc, {
+        '2 ^ 2': ('binop_power', _i(2), _i(2)),
+        '1 + 2 ^ 2': ('binop_plus', _i(1), ('binop_power', _i(2), _i(2))),
+        '1 / 2 ^ 2': ('binop_divide', _i(1), ('binop_power', _i(2), _i(2))),
+        '1 ^ 2 ^ 2': ('binop_power', _i(1), ('binop_power', _i(2), _i(2))),
+    })
+
+def test_binop_mod(calc):
+    input_should_match(calc, {
+        '2 % 2': ('binop_mod', _i(2), _i(2)),
+        '1 + 2 % 2': ('binop_plus', _i(1), ('binop_mod', _i(2), _i(2))),
+        '1 / 2 % 2': ('binop_mod', ('binop_divide', _i(1), _i(2)), _i(2)),
+        '1 % 2 ^ 2': ('binop_mod', _i(1), ('binop_power', _i(2), _i(2))),
+    })
+
+def test_binop_divide(calc):
+    input_should_match(calc, {
+        '2 / 2': ('binop_divide', _i(2), _i(2)),
+        '1 + 2 / 2': ('binop_plus', _i(1), ('binop_divide', _i(2), _i(2))),
+        '1 / 2 / 2': ('binop_divide', ('binop_divide', _i(1), _i(2)), _i(2)),
+        '1 / 2 ^ 2': ('binop_divide', _i(1), ('binop_power', _i(2), _i(2))),
+    })
+
+def test_binop_multiply(calc):
+    input_should_match(calc, {
+        '2 * 2': ('binop_multiply', _i(2), _i(2)),
+        '1 + 2 * 2': ('binop_plus', _i(1), ('binop_multiply', _i(2), _i(2))),
+        '1 / 2 * 2': ('binop_multiply', ('binop_divide', _i(1), _i(2)), _i(2)),
+        '1 * 2 ^ 2': ('binop_multiply', _i(1), ('binop_power', _i(2), _i(2))),
+    })
+
+def test_binop_minus(calc):
+    input_should_match(calc, {
+        '2 - 2': ('binop_minus', _i(2), _i(2)),
+        '1 + 2 - 2': ('binop_minus', ('binop_plus', _i(1), _i(2)), _i(2)),
+        '1 - 2 * 2': ('binop_minus', _i(1) ,('binop_multiply', _i(2), _i(2))),
+        '1 - 2 ^ 2': ('binop_minus', _i(1), ('binop_power', _i(2), _i(2))),
+    })
+
+def test_binop_plus(calc):
+    input_should_match(calc, {
+        '2 + 2': ('binop_plus', _i(2), _i(2)),
+        '1 + 2 + 2': ('binop_plus', ('binop_plus', _i(1), _i(2)), _i(2)),
+        '1 + 2 * 2': ('binop_plus', _i(1) ,('binop_multiply', _i(2), _i(2))),
+        '1 + 2 ^ 2': ('binop_plus', _i(1), ('binop_power', _i(2), _i(2))),
+    })
+
+def test_assignment(calc):
+    input_should_match(calc, {
+        'a = 1': ('assignment', _i(1), 'a'),
+        'a = 1 ^ 2': ('assignment', ('binop_power', _i(1), _i(2)), 'a'),
+        '42 % a = 1': ('binop_mod', _i(42), ('assignment', _i(1), 'a')),
+        'cos(a = 1)': ('function_expr', ('assignment', _i(1), 'a'), 'cos'),
+    })
+
+def test_negative_expr(calc):
+    input_should_match(calc, {
+        '-1': ('negative_expr', _i(1)),
+        '-sin(2)': ('negative_expr', ('function_expr', _i(2), 'sin')),
+        '-(2 % 3)': ('negative_expr', ('binop_mod', _i(2), _i(3))),
+        '2 + -3': ('binop_plus', _i(2), ('negative_expr', _i(3))),
+        '2 / -3': ('binop_divide', _i(2), ('negative_expr', _i(3))),
+        '2 ^ -3': ('binop_power', _i(2), ('negative_expr', _i(3))),
+    })
+
