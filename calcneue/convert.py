583d6af3de0af705d0a2fad09c87fafd92644e2a
Daniel Duan <danmarner@gmail.com>
2014-04-09 09:10:44 -0500

    replaced tabs with spaces

--- calcneue/convert.py
+++ calcneue/convert.py
@@ -1,15 +1,15 @@
 units = {
-	'kilometer': (1000, 'meter'),
-	'kilometers': (1000, 'meter'),
-	'kilometre': (1000, 'meter'),
-	'kilometres': (1000, 'meter'),
+    'kilometer': (1000, 'meter'),
+    'kilometers': (1000, 'meter'),
+    'kilometre': (1000, 'meter'),
+    'kilometres': (1000, 'meter'),
 }
 def convert(quantity, unit):
-	if quantity[1] == None:
-		return (quantity[0], unit)
-	else:
-		factor = units[quantity[1]]
-		return quantity[0] * factor[0], factor[1]
+    if quantity[1] == None:
+        return (quantity[0], unit)
+    else:
+        factor = units[quantity[1]]
+        return quantity[0] * factor[0], factor[1]
 
 def lookup_base_unit(unit):
-	return None
+    return None
