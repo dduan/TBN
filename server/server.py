780c6acfb01395c2b22508df39c6ab688283a799
Daniel Duan <danmarner@gmail.com>
2014-04-15 18:43:29 -0500

    adapted server code to server static files

--- server/server.py
+++ server/server.py
@@ -1,16 +1,11 @@
 from flask import request, Flask, jsonify, json
 
 import os
-app = Flask(__name__, static_folder='./')
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-	return app.send_static_file('client/index.html')
-
-@app.route('/client/<path:path>')
-def static_proxy(path):
-	print(path)
-	return app.send_static_file(os.path.join('client', path))
+	return app.send_static_file('static/index.html')
 
 #TODO: replace tbn() with the real deal
 @app.route('/api', methods=['GET'])
