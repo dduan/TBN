3e67645c528a16dd2d0aefccb1f3c1b30fd7e75f
Daniel Duan <danmarner@gmail.com>
2014-04-18 13:32:58 -0500

    replaced some tabs with spaces

--- webapp/server.py
+++ webapp/server.py
@@ -5,15 +5,15 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
-	return app.send_static_file('index.html')
+    return app.send_static_file('index.html')
 
 #TODO: replace tbn() with the real deal
 @app.route('/api', methods=['GET'])
 def get_request():
-	return jsonify({'result' : tbn(request.args['input'])})
+    return jsonify({'result' : tbn(request.args['input'])})
 
 def tbn(body):
-	return ["<3" for x in body.split('\n')]
+    return ["<3" for x in body.split('\n')]
 
 if __name__ == '__main__':
     app.run()
