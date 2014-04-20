719b69dd687f745d6ad8c226051061a24f4c2560
Daniel Duan <danmarner@gmail.com>
2014-04-20 16:29:01 -0500

    adapted convert to new complex unit format

--- calcneue/convert.py
+++ calcneue/convert.py
@@ -10,29 +10,29 @@ def convert(quantity, unit):
     # 1 in meter, unit should be meter
     if quantity[1] == None:
         if unit == None:
-            return (quantity[0], ([], []))
+            return (quantity[0], (set(), set()))
         else:
-            return (quantity[0], ([(unit, 1)], []))
+            return (quantity[0], ({(unit, 1)}, set()))
     else: 
         try:
             # found relation
             relation = relations[quantity[1]]
-            return quantity[0] * relation[0] + relation[1], ([(relation[2],1)],[])
+            return quantity[0] * relation[0] + relation[1], ({(relation[2],1)},set())
         except KeyError:
             try:
                     # found inverse relation
                     relation = relations[unit]
-                    return (quantity[0] - relation[1]) / relation[0], ([(unit, 1)],[])
+                    return (quantity[0] - relation[1]) / relation[0], ({(unit, 1)},set())
             except KeyError:
                 # can't find relation, keep old unit
-                return quantity[0], ([(quantity[1], 1)], [])
+                return quantity[0], ({(quantity[1], 1)}, set())
 
 def lookup_base_unit(unit):
     '''also takes care of aliases 'm' => "meter"'''
     if unit == 'meters':
-    	return 'meter'
+        return 'meter'
     else: 
-    	return unit
+        return unit
 
 if __name__ == '__main__':
     while True:
