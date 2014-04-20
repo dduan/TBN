c0a5b187a596d889304a158c3063ed955a787660
Daniel Duan <danmarner@gmail.com>
2014-04-20 12:13:04 -0500

    updated reduce_unit with new definiation of complex unit

--- calcneue/reduce_unit.py
+++ calcneue/reduce_unit.py
@@ -1,25 +1,21 @@
 '''
 internal representation of a unit during reduce:
-	(
-	[(unit, deg), (unit,deg), ...], # list of numerators
-	[(unit, deg), (unit,deg), ...]  # list of denominators
-	)
+    (
+    {(unit, deg), (unit,deg), ...}, # set of numerators
+    {(unit, deg), (unit,deg), ...}  # set of denominators
+    )
 '''
 def unit_is_empty(unit):
     ''' empty or None'''
-    return unit==([], [])
+    return unit == (set(), set())
 
 def unit_is_equal(a, b):
-    return a==b
+    return a == b
 
 def simplify_unit(unit):
     pass
 
 def unit_is_complex(unit):
-    ''' numerator has one unit with degree 1, denominator is empty'''
-    #print(len(unit[0]))
-    #print(len(unit[1]))
-    #print(unit[0][0][1])
-    if len(unit[0]) == 1 and unit[0][0][1] == 1 and len(unit[1]) == 1 and len(unit[1][0]) == 0:
-        print("unit is complex")
-        return True
+    '''not (numerator has one unit with degree 1, denominator is empty)'''
+    simple = len(unit[0]) == 1 and list(unit[0])[0][1] == 1 and not len(unit[1])
+    return not simple and not unit_is_empty(unit)
