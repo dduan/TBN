027b4a436c05995441211c0d18808d99ffdc63b0
Daniel Duan <danmarner@gmail.com>
2014-04-20 01:12:51 -0500

    added tests for assignment and variable evaluations

--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -38,22 +38,34 @@ def test_reduce_convert_expr(red):
 
 def test_binop_plus(red):
     input_should_match(red, {
-        ('binop_plus', ('quantity', ('integer_number', 1), None), ('quantity', ('integer_number', 2), None)): (3, _sunit(None)),
+        ('binop_plus', ('quantity', ('integer_number', 1), None), ('quantity', ('integer_number', 2), None)): (3, _sunit()),
         })
 
 def test_binop_power_to_quantity_with_unit(red):
     '''1 meter ^ 2 foot'''
     input_should_match(red, {
-    ('binop_power', ('quantity', _i(1), 'meter'), ('quantity', _i(1), 'apple')): (None, _sunit(None)),
+    ('binop_power', ('quantity', _i(1), 'meter'), ('quantity', _i(1), 'apple')): (None, _sunit()),
     })
 
 def test_reduce_basic_math_function(red):
     # sin(PI) should be 0
     node =  ('function_expr', ('quantity', ('float_number', 3.1415926), None), 'sin')
-    print(red(node))
     assert _almost_equal(red(node)[0], 0)
     # sin(PI/2 meter) should be 1 meter
     node =  ('function_expr', ('quantity', ('float_number', 3.1415926/2), 'meter'), 'sin')
     result = red(node)
     assert _almost_equal(result[0], 1)
     assert result[1] == _sunit('meter')
+
+def test_assignment_expr_evaluation(red):
+    assert red(('assignment', ('quantity', ('integer_number', 1), 'meter'), 'b')) == (1, _sunit('meter'))
+
+def test_varialbe_evaluation(red):
+    context = { 'a': (1, _sunit()) }
+    assert reduce(context, ('variable', 'a', None)) == (1, _sunit())
+    assert reduce(context, ('variable', 'b', None)) == (None, _sunit())
+
+def test_assignment_side_effect(red):
+    # '(a=1) + a' should be 2
+    node = ('binop_plus', ('assignment', ('quantity', ('integer_number', 1), None), 'a'), ('quantity', ('integer_number', 1), None))
+    assert red(node) == (2, _sunit())
